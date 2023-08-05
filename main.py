a0bea18c9c9e9d4bfed0caf199293da95492e73b
Tiago Custódio <132753395+tiago3186@users.noreply.github.com>
2023-08-05 14:39:35 -0300

    UPDATE main menu

--- main.py
+++ main.py
@@ -12,23 +12,19 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Ship War")
 
-    #custom_font = pygame_menu.font.FONT_OPEN_SANS
-
     my_custom_theme = pygame_menu.themes.Theme(
-    #font=custom_font,
-    title_font_size=50,
-    title_font_color=(255, 255, 255),
-    widget_font_size=30,
-    widget_font_color=(255, 255, 255),
-    background_color=(0, 0, 0),
-    selection_color=(255, 0, 0),
-    title_offset=(0, 50)
+        title_font_size=50,
+        title_font_color=(255, 255, 0),  # Amarelo (R, G, B)
+        widget_font_size=30,
+        widget_font_color=(255, 255, 255),
+        background_color=(0, 0, 0),
+        selection_color=(255, 0, 0),
+        title_offset=(SCREEN_WIDTH // 2.7, 0)  # Centralizar horizontalmente
     )
 
     menu = pygame_menu.Menu('SHIP WAR', SCREEN_WIDTH, SCREEN_HEIGHT, theme=my_custom_theme)
 
-    
-    menu.add.button('NOVO JOGO', start_game)  # Chamada para a função start_game
+    menu.add.button('NOVO JOGO', start_game)
     menu.add.button('SAIR', pygame_menu.events.EXIT)
 
     menu.mainloop(screen)
